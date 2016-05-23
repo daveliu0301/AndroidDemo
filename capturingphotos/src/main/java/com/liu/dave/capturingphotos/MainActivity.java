1d671309a038f49451a0259f2d8e06acf7cb98d4
daveliu0301 <daveliu0301@gmail.com>
2016-05-23 17:27:09 +0800

    take full-size photo

--- capturingphotos/src/main/java/com/liu/dave/capturingphotos/MainActivity.java
+++ capturingphotos/src/main/java/com/liu/dave/capturingphotos/MainActivity.java
@@ -2,32 +2,99 @@ package com.liu.dave.capturingphotos;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.liu.dave.capturingphotos.databinding.ActivityMainBinding;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private MainViewModel viewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.setViewModel(new MainViewModel(this));
+        viewModel = new MainViewModel(this);
+        binding.setViewModel(viewModel);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK)
+            if (requestCode == MainViewModel.REQUEST_THUMBNAIL_CAPTURE) {
+                Bitmap bm = getThumbnail(data);
+                binding.picture.setImageBitmap(bm);
 
-        if (requestCode == MainViewModel.REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap picture = (Bitmap) extras.get("data");
-            binding.picture.setImageBitmap(picture);
+            } else if (requestCode == MainViewModel.REQUEST_IMAGE_CAPTURE) {
+                galleryAddPic();
+                setPic();
+            }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case MainViewModel.PERMISSION_REQUEST:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "permission granted, try again.", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "permission denied, can not take photo.", Toast.LENGTH_SHORT).show();
+
+                }
+                return;
         }
     }
+
+    private Bitmap getThumbnail(Intent data) {
+        Bundle extras = data.getExtras();
+        Bitmap picture = (Bitmap) extras.get("data");
+        return picture;
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        Uri photoUri = viewModel.getPhotoUri();
+        mediaScanIntent.setData(photoUri);
+        sendBroadcast(mediaScanIntent);
+    }
+
+    private void setPic() {
+        String photoPath = viewModel.getPhotoUri().getPath();
+
+        int targetW = binding.picture.getWidth();
+        int targetH = binding.picture.getHeight();
+
+        BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(photoPath, bmOptions);
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        int scaleFactor = Math.min(targetH / photoH, targetW / photoW);
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(photoPath, bmOptions);
+        binding.picture.setImageBitmap(bitmap);
+    }
+
+
 }
--- capturingphotos/src/main/java/com/liu/dave/capturingphotos/MainViewModel.java
+++ capturingphotos/src/main/java/com/liu/dave/capturingphotos/MainViewModel.java
@@ -1,34 +1,83 @@
 package com.liu.dave.capturingphotos;
 
+import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.database.Observable;
-import android.databinding.BindingAdapter;
-import android.databinding.ObservableField;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Picture;
-import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
-import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by LiuDong on 2016/5/9.
  */
 public class MainViewModel {
     public static final int REQUEST_IMAGE_CAPTURE = 1;
-    private Context context;
+    public static final int REQUEST_THUMBNAIL_CAPTURE = 2;
+    private Activity activity;
+    private Uri photoUri;
 
-    public MainViewModel(Context context) {
-        this.context = context;
+    public MainViewModel(Activity activity) {
+        this.activity = activity;
     }
 
-    public void requestCamera(View view) {
+    public void dispatchTakeThumbnailIntent(View view) {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(context.getPackageManager()) != null)
-            ((Activity) context).startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null)
+            activity.startActivityForResult(takePictureIntent, REQUEST_THUMBNAIL_CAPTURE);
     }
 
+    public void dispatchTakePictureIntent(View view) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(activity.getPackageManager()) != null) {
+            File photo = null;
+            try {
+                photo = createImageFile();
+            } catch (IOException e) {
+                checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            }
+            if (photo != null) {
+                photoUri = Uri.fromFile(photo);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                activity.startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
+        }
+
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        return image;
+    }
+
+    public Uri getPhotoUri() {
+        return photoUri;
+    }
+
+    public static final int PERMISSION_REQUEST = 1;
+
+    private void checkPermission(String permission) {
+        int permissionCheck = ContextCompat.checkSelfPermission(activity, permission);
+        if (PackageManager.PERMISSION_DENIED == permissionCheck) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(activity, permission)) {
+                Toast.makeText(activity, "no \"" + permission + "\" permission", Toast.LENGTH_SHORT).show();
+            } else {
+                ActivityCompat.requestPermissions(activity, new String[]{permission}, PERMISSION_REQUEST);
+            }
+        }
+
+    }
 }
